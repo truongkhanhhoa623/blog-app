aefeed3ac6e80a3b50f6fc345a57563da6065668
khanhhoa <truongkhanhhoa623@gmail.com>
2021-10-07 23:06:44 +0700

    deploy

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('client/build'))
-app.use('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.use(express.static(path.join(__dirname, 'build')))
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 app.listen(PORT, ()=>{console.log('listening on port 5000')})
