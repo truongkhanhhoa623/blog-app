ca75609bd7641b80e494b122e0b8e026a7ac2d8a
khanhhoa <truongkhanhhoa623@gmail.com>
2021-10-07 22:59:20 +0700

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('client/build'))
-app.use( (req, res) => {
+app.use('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
 
